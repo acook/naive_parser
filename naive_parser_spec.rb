bb041324272146f581b197820d6d51f96c4a442d
Anthony Cook <anthonymichaelcook@gmail.com>
2011-08-24 21:24:39 -0700

    Add an exe test, not passing yet.

--- naive_parser_spec.rb
+++ naive_parser_spec.rb
@@ -21,4 +21,13 @@ describe 'naive_parser executable' do
     status = Open4.popen4(exe) {}
     status.to_i.should eq(256)
   end
+
+  it "doesn't fail when you pass in a file" do
+    pinfo = Hash.new
+    status = Open4.popen4(exe, test_file) do |pid, stdin, stdout, stderr|
+      pinfo = {:pid => pid, :stdout => stdout.read, :stderr => stderr.read}
+    end
+
+    status.to_i.should eq(0), "## status: #{status.inspect} \n## pinfo: #{pinfo.inspect}"
+  end
 end
